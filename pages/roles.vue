3659398673692a9faac5c13880a61fd7a299fb3a
Nate Butler <iamnbutler@gmail.com>
2021-10-31 23:25:56 -0400

    Add content

--- pages/roles.vue
+++ pages/roles.vue
@@ -10,9 +10,8 @@
       design? Or do I specialize in one thing like brand design, prototyping?"
     </p>
     <p>
-      "Should I learn to code? Write? How about learning 3D? Business?
-      There seems to be an endless amount of things to learn, and my
-      time is limited."
+      "Should I learn to code? Write? How about learning 3D? Business? There
+      seems to be an endless amount of things to learn, and my time is limited."
     </p>
     <p>
       While opinions varry, I'm going to put a path forward while explaining the
@@ -20,13 +19,52 @@
     </p>
 
     <h2>Design roles</h2>
+    <p>
+      A good way to get an overview of what different types of designers do in
+      their day-to-day is to look at an overview of the roles found in the
+      (digial) design industry.
+    </p>
+    <p>
+      While these won't be definitive introductions to each role, a good way to
+      get a further feel for each role is to look at job postings for those
+      roles on places like design job boards and Linkedin.
+    </p>
+    <p>
+      Some larger tech companies have role descriptions seperately from their
+      job postings. Reading through these might give some useful clues towards
+      what types of roles you might enjoy.
+    </p>
     <h3>General Design Roles</h3>
-    <Collapsible>
-      <template v-slot:title>
-        Product Design
+    <Collapsible expanded>
+      <template v-slot:title>Product Design</template>
+      <template v-slot:content>
+        <p>
+          Product design is an umbrella term used in the industry for a designer
+          who covers all, or close to all roles needed to ship a digital product
+          (app, mobile app, website.)
+        </p>
+        <p>
+          We'll discuss further below how this term often used to mean many
+          things, and how you can figure out what a posted role is looking for.
+        </p>
       </template>
-      <template v-slot:content> hello world </template>
     </Collapsible>
+    <Collapsible expanded>
+      <template v-slot:title>Visual Design</template>
+      <template v-slot:content>
+        <p>
+          As the name implies, a visual designer focuses on the visual
+          presentation of a product. A visual designer will own a product's
+          presentation,
+        </p>
+        <p>
+          From the perspective of the tech industry, visual design is an
+          uncommon role (with some exceptions, notably
+          <a href="https://design.google/jobs/visual-designer/">Google</a>.)
+        </p>
+      </template>
+    </Collapsible>
+
     <ul>
       <li>
         Product Design
