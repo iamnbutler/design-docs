804a020e71113b8a96d3a8ca261e86a5095785a4
Nate Butler <iamnbutler@gmail.com>
2021-11-01 00:13:37 -0400

    Add FooterNav – A component for previous/next links

---
+++ components/FooterNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="nav-footer flex flex-row justify-between mt-8">
+    <div class="prev-link">
+      <NuxtLink v-if="prev" :to="prevLink" class="inline-block border-b-4" active-class="active"
+        >&larr; {{ prevText }}</NuxtLink
+      >
+    </div>
+    <div class="next-link">
+      <NuxtLink v-if="next" :to="nextLink" class="inline-block border-b-4" active-class="active"
+        >{{ nextText }} &rarr;</NuxtLink
+      >
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    prev: Boolean,
+    prevLink: String,
+    prevText: String,
+    next: Boolean,
+    nextLink: String,
+    nextText: String
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+a:not(.active) {
+  border-bottom-color: transparent;
+  &:hover {
+    border-bottom-color: #34d399;
+  }
+}
+
+a.active {
+  border-bottom-color: #34d399;
+  &:hover {
+    border-bottom-color: #10b981;
+  }
+}
+.nav-footer a {
+    text-decoration: none;
+}
+</style>
