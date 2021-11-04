d6db9a87dfebb433299b3cf94ed7a297a05ef896
Nate Butler <iamnbutler@gmail.com>
2021-11-04 09:39:22 -0400

    Add meta + favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
-    css: ['tailwindcss/tailwind.css'],
-    build: {
-      postcss: {
-        postcssOptions: require('./postcss.config.js'),
-      },
+  css: ["tailwindcss/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
     },
-  })
+  },
+  head: {
+    title: "Getting your first job in the product design industry",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "my website description",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+});
