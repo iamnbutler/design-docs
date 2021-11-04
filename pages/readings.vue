89137d513c72704017566c1b125340b6b9b0fe50
Nate Butler <iamnbutler@gmail.com>
2021-11-04 12:08:26 -0400

    Add some additional readings

--- pages/readings.vue
+++ pages/readings.vue
@@ -1,17 +1,21 @@
 <template>
   <article>
     <h2>Further Readings</h2>
-    <p>Work in progress, more coming here soon!</p>
+    <p>Work in progress, more readings and descriptions coming soon!</p>
     <h3>Readings</h3>
     <ul>
       <li><a href="https://readymag.com/shuffle/dieter-rams/">Dieter Rams: 10 Principles for Good Design</a></li>
+      <li><a href="https://medium.com/the-year-of-the-looking-glass/finding-ideas-d9c07ca1a076">Finding Ideas</a>– Julie Zhou</li>
       <li><a href="https://m.signalvnoise.com/why-i-love-ugly-messy-interfaces-and-you-probably-do-too/">Why I love ugly, messy interfaces — and you probably do too</a>  – Jonas Downey</li>
       <li><a href="https://medium.com/docusign-design/5-tips-for-an-outcome-driven-design-portfolio-d72ed6f51d12">5 Tips for an Outcome Driven Design Portfolio</a></li>
     </ul>
     <h3>Books</h3>
     <ul>
-        <li><a href="http://thinkingwithtype.com/">Thinking with Type – Ellen Lupton</a></li>
-        <li><a href="http://thinkingwithtype.com/">Graphic Design: The New Basics – Ellen Lupton and Jennifer Cole Phillips</a></li>
+        <li><a href="http://thinkingwithtype.com/">Graphic Design: The New Basics</a> – Ellen Lupton and Jennifer Cole Phillips</li>
+        <li><a href="https://sensible.com/dont-make-me-think/">Don't Make Me Think</a>  – Steve Krug</li>
+        <li><a href="https://www.amazon.ca/Design-Everyday-Things-Revised-Expanded/dp/0465050654/">The Design of Everyday Things</a> – Don Norman</li>
+        <li><a href="https://austinkleon.com/steal/">Steal like an Artist</a>  – Austin Kleon</li>
+        <li><a href="http://thinkingwithtype.com/">Thinking with Type</a>  – Ellen Lupton</li>
     </ul>
     <h3>Resources</h3>
     <ul>
