0bc1a59cbd9b08e21ebcde764484f7a53ce7a492
Nate Butler <iamnbutler@gmail.com>
2021-11-04 12:08:19 -0400

    Update talk & work

--- pages/talk.vue
+++ pages/talk.vue
@@ -1,20 +1,22 @@
 <template>
   <article>
     <h2>Talk about your work</h2>
+    <p>Now that you have a portfolio, it's time to talk about your work.</p>
+    <p>Whether interviewing, writing an article, or revising your project write-ups,  knowing how to talk about your work is vital for a designer.</p>
     <ul>
       <li>
         <a
           href="https://brianlovin.com/writing/how-to-give-a-great-product-design-portfolio-presentation"
-          >How to give a great product design portfolio presentation – Brian
-          Lovin</a
+          >How to give a great product design portfolio presentation</a
         >
+        – Brian Lovin
       </li>
       <li>
         <a
           href="https://uxdesign.cc/three-ways-to-more-effectively-present-your-designs-cef559c3461f"
-          >Three ways to more effectively present your designs – Tanner
-          Christensen</a
+          >Three ways to more effectively present your designs</a
         >
+        – Tanner Christensen
       </li>
     </ul>
     <FooterNav
@@ -30,6 +32,6 @@
 
 <script>
 export default {
-  scrollToTop: true
-}
+  scrollToTop: true,
+};
 </script>
