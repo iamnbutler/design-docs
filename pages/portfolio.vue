d292e497b1938000ca84acc30ed07dd1c3560508
Nate Butler <iamnbutler@gmail.com>
2021-11-04 01:01:32 -0400

    Reorganize portfolio and add links

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,9 +1,43 @@
 <template>
   <article>
-    <h2>First</h2>
+    <h2>Don't over-invest in your website</h2>
     <p>
-      Read <a href="https://essays.uxdesign.cc/case-study-factory/">this</a>.
+      Your portfolio doesn't have to be unique or ornate, as long as it acts as
+      an effective vehicle to share your work.
     </p>
+    <p>
+      The number one mistake designers just coming out of school make is
+      spending far too much time creating a portfolio with hardly any work in
+      it.
+    </p>
+    <p>
+      Try spending no more than a week on your first portfolio, and check in on
+      how you are investing your time by roughly using a 1:4 ratio of time spent
+      building your portfolio to working on projects.
+    </p>
+    <p>
+      Your portfolio will be with you for a long time, and you will end up
+      changing it frequently. It's ok to invest time in a portfolio website but
+      only do so once you are <b>sure</b> you have at least three strong
+      projects to show.
+    </p>
+
+    <p>Before moving on, read these:</p>
+
+    <ul>
+      <li>
+        <a href="https://essays.uxdesign.cc/case-study-factory/"
+          >the case study factory</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://medium.com/docusign-design/5-tips-for-an-outcome-driven-design-portfolio-d72ed6f51d12"
+          >5 Tips for an Outcome Driven Design Portfolio</a
+        >
+      </li>
+    </ul>
+
     <h2>The Basics</h2>
     <ul>
       <li>
@@ -29,22 +63,37 @@
         design projects in your portfolio.
       </li>
     </ul>
-    <h2>Don't over-invest in your website</h2>
-    <p>
-      Your portfolio doesn't have to be unique or ornate, as long as it acts as an effective vehicle to share your work. 
-    </p>
-    <p>
-      The number one mistake designers just coming out of school make is spending far too much time creating a portfolio with hardly any work in it.
-    </p>
-    <p>Try spending no more than a week on your first portfolio, and check in on how you are investing your time by roughly using a 1:4 ratio of time spent building your portfolio to working on projects.</p>
-    <p>Your portfolio will be with you for a long time, and you will end up changing it frequently. It's ok to invest time in a portfolio website but only do so once you are <b>sure</b> you have at least three strong projects to show.</p>
     <h2>Getting started</h2>
+    <p>
+      If you are feeling overwhelmed on where to start, below is a guide to get
+      started with the simplest (yet still effective!) portfolio.
+    </p>
     <ol>
-      <li>Create a <a href="https://read.cv/explore">read.cv</a>. This can act as both an initial portfolio and an online resume.</li>
-      <li><a href="https://vanschneider.com/blog/portfolio-tips/write-project-case-studies-portfolio/">Read this</a>, then write up the projects you have, up to three.</li>
-      <li>Try to keep each under 750 words (around ~5 minutes of reading.) Have a summary of the project right at the top.</li>
-      <li>Each time you finish a project, do a quick write-up while it is fresh in your mind. Before long, you will have a ton of work to share when you create a complete portfolio site.</li>
-      <li><a href="https://read.cv/andy">Andy Chung's</a> cv is a great example of what you are going for. <a href="https://read.cv/andy/X2KMqw0jLQhaeavZHPK5">This project</a> is exactly the length you are going for.</li>
+      <li>
+        Create a <a href="https://read.cv/explore">read.cv</a>. This can act as
+        both an initial portfolio and an online resume.
+      </li>
+      <li>
+        <a
+          href="https://vanschneider.com/blog/portfolio-tips/write-project-case-studies-portfolio/"
+          >Read this</a
+        >, then write up the projects you have, up to three.
+      </li>
+      <li>
+        Try to keep each under 750 words (around ~5 minutes of reading.) Have a
+        summary of the project right at the top.
+      </li>
+      <li>
+        Each time you finish a project, do a quick write-up while it is fresh in
+        your mind. Before long, you will have a ton of work to share when you
+        create a complete portfolio site.
+      </li>
+      <li>
+        <a href="https://read.cv/andy">Andy Chung's</a> cv is a great example of
+        what you are going for.
+        <a href="https://read.cv/andy/X2KMqw0jLQhaeavZHPK5">This project</a> is
+        exactly the length you are going for.
+      </li>
     </ol>
     <FooterNav
       prev
