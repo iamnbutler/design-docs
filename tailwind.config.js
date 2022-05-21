8535f33abd6cc06885bd7aa97f9c09bc90a85e23
Nate Butler <iamnbutler@gmail.com>
2022-05-21 14:53:02 -0400

    WIP: Begin styling tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Provide 16 hex values from a base16 theme to update the system's theme
+// Some examples of base16 themes here: https://github.com/chriskempson/base16
+
 const theme = {
   base00: "#191724",
   base01: "#1f1d2e",
@@ -14,11 +17,19 @@ const theme = {
   base0C: "#31748f",
   base0D: "#9ccfd8",
   base0E: "#c4a7e7",
-  base0F: "#e5e5e5"
+  base0F: "#e5e5e5",
 };
 
-// Provide 16 hex values from a base16 theme to update the system's theme
-// Some examples of base16 themes here: https://github.com/chriskempson/base16
+// Utilities from tailwind typography's default config
+// source: https://github.com/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
+
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
 
 module.exports = {
   content: [
@@ -30,7 +41,54 @@ module.exports = {
     extend: {
       colors: theme,
     },
+    typography: {
+      DEFAULT: {
+        css: {
+          color: theme.base07,
+          fontSize: rem(16),
+          lineHeight: round(28 / 16),
+          // Headlines
+          h1: {
+            color: theme.base09,
+          },
+          // Body
+          p: {
+            marginTop: em(16, 14),
+            marginBottom: em(16, 14),
+          },
+          a: {
+            color: theme.base09,
+            "&:hover": {
+              color: theme.base0B,
+            },
+          },
+          // Lists
+          ol: {
+            marginTop: em(20, 16),
+            marginBottom: em(20, 16),
+            paddingLeft: em(26, 16),
+          },
+          ul: {
+            marginTop: em(20, 16),
+            marginBottom: em(20, 16),
+            paddingLeft: em(26, 16),
+          },
+          li: {
+            marginTop: em(8, 16),
+            marginBottom: em(8, 16),
+          },
+          // Dividers
+          hr: {
+            borderColor: theme.base03,
+            marginTop: em(48, 16),
+            marginBottom: em(48, 16),
+          },
+          'hr + *': {
+            marginTop: '0',
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
-
