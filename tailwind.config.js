a3923f2740ebece94e21689deadf033e4b7ef62f
Nate Butler <iamnbutler@gmail.com>
2022-05-20 21:33:08 -0400

    init TailwindCSS
    - Also fix an incorrect directory on the `npm dev:watch` script

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./content/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
