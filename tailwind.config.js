2b6580e4f702393fe955c5f9a20760410c9c6d6b
Nate Butler <iamnbutler@gmail.com>
2022-05-21 14:45:23 -0400

    use `theme` to generate extended tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,25 @@
+const theme = {
+  base00: "#191724",
+  base01: "#1f1d2e",
+  base02: "#26233a",
+  base03: "#555169",
+  base04: "#6e6a86",
+  base05: "#e0def4",
+  base06: "#f0f0f3",
+  base07: "#c5c3ce",
+  base08: "#e2e1e7",
+  base09: "#eb6f92",
+  base0A: "#f6c177",
+  base0B: "#ebbcba",
+  base0C: "#31748f",
+  base0D: "#9ccfd8",
+  base0E: "#c4a7e7",
+  base0F: "#e5e5e5"
+};
+
+// Provide 16 hex values from a base16 theme to update the system's theme
+// Some examples of base16 themes here: https://github.com/chriskempson/base16
+
 module.exports = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
@@ -6,25 +28,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        base00: "#191724",
-        base01: "#1f1d2e",
-        base02: "#26233a",
-        base03: "#555169",
-        base04: "#6e6a86",
-        base05: "#e0def4",
-        base06: "#f0f0f3",
-        base07: "#c5c3ce",
-        base08: "#e2e1e7",
-        base09: "#eb6f92",
-        base0A: "#f6c177",
-        base0B: "#ebbcba",
-        base0C: "#31748f",
-        base0D: "#9ccfd8",
-        base0E: "#c4a7e7",
-        base0F: "#e5e5e5",
-      },
+      colors: theme,
     },
   },
   plugins: [require("@tailwindcss/typography")],
 };
+
